21f0376b5658a44cfcf9308fa492b0f5d7b20d71
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2022-10-03 22:40:56 -0400

    add initial files for project

---
+++ geneticmusic/main.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from rich.console import Console
+import typer
+
+# create a Typer object to support the command-line interface
+cli = typer.Typer()
+@cli.command()
+def main(myParameter: str = "", myFile: str = ""):
+    """determine parameter"""
+    console = Console()
+    console.print("Welcome to GeneticMusic {:music:}")
+    console.print(f"   myParameter = {myParameter}")
+
+# end of main()
+
