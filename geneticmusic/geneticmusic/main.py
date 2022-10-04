2a60a500ebf6ef90ca479b2e734676bc3db05051
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2022-10-04 00:48:45 -0400

    update file struc

--- geneticmusic/geneticmusic/main.py
+++ geneticmusic/geneticmusic/main.py
@@ -1,16 +1,37 @@
-from rich.console import Console
+"""Define the command-line interface for the datasummarizer program."""
+
+from pathlib import Path
+
 import typer
 
-# create a Typer object to support the command-line interface
+from rich.console import Console
+
 cli = typer.Typer()
 
+console = Console()
 
 @cli.command()
-def main(param: str = "", myFile: str = ""):
-    """determine parameter"""
-    console = Console()
-    console.print("Welcome to GeneticMusic :sparkles:")
-    console.print(f"   myParameter = {param}")
-    console.print(":brick:" * 20)
+
+def main(
+	param: str = "",
+    data_file: Path = typer.Option(...),
+):
+	"""Determine parameters"""
+	console.print("Welcome to GeneticMusic :sparkles:")
+	console.print(f"   myParameter = {param}")
+	console.print(f"   data_file = {data_file}")
+
+	# --> the file was not specified so we cannot continue using program
+	if data_file is None:
+		console.print("No data file specified!")
+		raise typer.Abort()
+	# --> the file was specified and it is valid so we should read and check it
+
+	if data_file.is_file():
+		console.print("\t [:microscope:] File appears to exist.")
+	elif not data_file.exists():
+		console.print("The data file does not exist!")
+
+	console.print(":brick:" * 20)
 
 # end of main()
